116f45ebf1feae874cce2e06342e2533b0027e3d
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-18 10:46:28 +0200

    Uppgift 9 klar

---
+++ 09 Support-ärenden/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Ange kodfelsnummer: ");
+
+if (int.TryParse(Console.ReadLine(), out int number))
+{
+    string kategory = number switch
+    {
+        >= 100 and <= 199 => "Hårdvarufel",
+        >= 200 and <= 299 => "Mjukvarufel",
+        _ => "Okänt fel"
+    };
+
+    Console.WriteLine($"Feltyp: {kategory}");
+}
+else
+{
+    Console.WriteLine("ERROR! Vänligen ange en giltid kod!");
+}
+
+Console.WriteLine("\n\nTryck på valfri tangent för att fortsätta...");
+Console.ReadKey();
