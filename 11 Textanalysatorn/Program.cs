605defce1c6935928bffc507064b87a4cc6a5154
hericktorrijo-cmd <hericktorrijo@gmail.com>
2026-09-18 21:49:17 +0200

    uppgift 11 klar

---
+++ 11 Textanalysatorn/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Skriv in en mening/text: ");
+string text  = Console.ReadLine().ToLower();
+
+if(string.IsNullOrWhiteSpace(text))
+{
+    Console.WriteLine("Texten är tom");
+}
+else if(text.StartsWith("hejsan"))
+{
+    Console.WriteLine("Texten börjar med hejsan");
+}
+else if(text.EndsWith("!"))
+{
+    Console.WriteLine("Texten slutar med utropstecken!");
+}
+else
+{
+    Console.WriteLine("Ingen matchning hittades!");
+}
+
+Console.WriteLine("\n\nTryck valfri tangent för att stänga konsolen");
+Console.ReadKey();
